2f15e2395174dc7efc3322604298a76e3579a1c4
Kei-Eff <karl.alberto@hotmail.com>
2021-11-19 09:44:02 +1100

    Added cost to keyboards model; some bootstrap

--- Control_Shift/models/keyboards.py
+++ Control_Shift/models/keyboards.py
@@ -4,6 +4,8 @@ class Keyboard(db.Model):
     __tablename__ = "keyboards"
     keyboard_id = db.Column(db.Integer, primary_key=True)
     keyboard_name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(200), default="No Description Provded")
+    cost = db.Column(db.Integer, nullable=False, server_default="0")
 
     def __init__(self, keyboard_name):
         self.keyboard_name = keyboard_name
--- Control_Shift/schemas/keyboard_schema.py
+++ Control_Shift/schemas/keyboard_schema.py
@@ -1,12 +1,13 @@
 from main import ma
 from models.keyboards import Keyboard
 from marshmallow_sqlalchemy import auto_field
-from marshmallow.validate import Length
+from marshmallow.validate import Length, Range
 
 class KeyboardSchema(ma.SQLAlchemyAutoSchema):
     keyboard_id = auto_field(dump_only=True)
     keyboard_name = auto_field(required=True, validate=Length(min=1))
-
+    description = auto_field(validate=Length(min=1))
+    cost = auto_field(required=False, validate=Range(0, 500))
     class Meta:
         model = Keyboard
         load_instance = True
